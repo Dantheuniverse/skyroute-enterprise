70eb3f3b93e5d5506bb56ef7acd8f748217217be
Danieltheflukr <haveanewlife@gmail.com>
2025-11-27 18:11:59 +0800

    Update astro.config.mjs

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: process.env.SITE_URL || 'https://skyroute.mingleedan.org',
+  site: process.env.SITE_URL || 'https://drone.mingleedan.org',
   build: {
     format: 'directory'
   }
